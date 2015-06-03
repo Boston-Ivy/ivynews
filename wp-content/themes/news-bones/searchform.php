6a89a9bb000bd084b745c870eae2ee6efb0dc636
Jase Warner <jase@jasewarner.com>
2015-06-03 10:21:30 +0100

    IE9+ fix for "Go" button in header

--- wp-content/themes/news-bones/searchform.php
+++ wp-content/themes/news-bones/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
-        <input type="search" id="s" name="s" value="" placeholder="Search site" />
-        <button class="green-btn" type="submit" id="searchsubmit" ><?php _e('Go','bonestheme'); ?></button>
+        <input type="search" id="s" name="s" class="search-field" value="" placeholder="Search site" />
+        <input type="submit" id="searchsubmit" class="btn btn-green" value="Go" />
     </div>
 </form>
