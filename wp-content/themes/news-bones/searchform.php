39f22948eda451611d32fb0b225e861bf9896084
Jase Warner <jase@jasewarner.com>
2015-06-10 12:57:48 +0100

    Changed home newsletter signup field

--- wp-content/themes/news-bones/searchform.php
+++ wp-content/themes/news-bones/searchform.php
@@ -2,6 +2,6 @@
     <span class="search-icon"><?php _e('Search'); ?></span>
     <div class="field-group">
         <input type="search" id="s" name="s" class="search-field" value="" placeholder="Search site" />
-        <input type="submit" id="searchsubmit" class="btn btn-green" value="Go" />
+        <input type="submit" id="searchsubmit" class="btn btn-green" value="Sign up now" />
     </div>
 </form>
