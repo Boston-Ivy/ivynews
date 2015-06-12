d40aaa13ad5021b6d684317177afb1a54b87f8c1
Jase Warner <jase@jasewarner.com>
2015-06-12 12:38:54 +0100

    Added annotation to page-home.php

--- wp-content/themes/news-bones/page-home.php
+++ wp-content/themes/news-bones/page-home.php
@@ -20,47 +20,41 @@
                         $hero_post = get_option("front_page_hero_story");
 
                         if ($hero_post) {
-
                             $post = get_post($hero_post);
                             $hero_cat = get_the_category($hero_post);
                             $parent_cats = get_category_parents($hero_cat[0]->cat_ID);
                             $split_arr = split("/", $parent_cats);
                             $exclude_cat = get_cat_id($split_arr[0]);
                             $hero_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'bones-thumb-450' );
-
                         ?>
 
                         <div id="Hero">
 
                             <div class="m-all t2of2 d2of2 herotext">
-
                                 <h1><a href="<?php echo get_permalink($post->ID); ?>">
 
                                         <?php
-                                            $intro = rwmb_meta( 'rw_tagline');
 
-                                            if (!empty($intro)) {
-                                                _e('<span>'); echo $excerpt = $intro; _e(': </span>');
-                                            }
+                                        $intro = rwmb_meta( 'rw_tagline');
 
-                                            echo $post->post_title
+                                        if (!empty($intro)) {
+                                            _e('<span>'); echo $excerpt = $intro; _e(': </span>');
+                                        }
+
+                                        echo $post->post_title
+
                                         ?>
 
                                 </a></h1>
-
                             </div>
 
                             <div class="m-all t-1of2 d-1of2 cf herotext">
-
                                 <p><?php echo rwmb_meta( 'rw_intro'); ?></p>
                                 <p><a href="<?php echo get_permalink($post->ID); ?>"><?php _e('Read more &raquo;'); ?></a></p>
-
                             </div>
 
                             <div class="m-all t-1of2 d-1of2 last-col cf heroimg">
-
                                 <a href="<?php echo get_permalink($post->ID); ?>" class="hero-thumb" title="<?php echo $post->post_title ?>" style="background-image: url(<?php echo $hero_image[0]; ?>);"></a>
-
                             </div>
 
                         </div>
@@ -104,17 +98,19 @@
                                     </script>
 
                                     <?php
+
                                     $editors_choice = get_option("front_page_editors_choice");
-                                    if($editors_choice){
-                                    $post = get_post($editors_choice);
-                                    ?>
 
-                                    <div class="entry editors-choice">
+                                    if ($editors_choice) {
 
-                                        <h3><span><?php _e("Editor's choice: "); ?></span><a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title ?></a></h3>
-                                        <p><?php $string = rwmb_meta( 'rw_intro'); $trimmedText = shorten_string($string, 18); echo $trimmedText ?></p>
+                                        $post = get_post($editors_choice); ?>
 
-                                    </div>
+                                        <div class="entry editors-choice">
+
+                                            <h3><span><?php _e("Editor's choice: "); ?></span><a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title ?></a></h3>
+                                            <p><?php $string = rwmb_meta( 'rw_intro'); $trimmedText = shorten_string($string, 18); echo $trimmedText ?></p>
+
+                                        </div>
 
                                     <?php } ?>
 
@@ -148,13 +144,9 @@
                             $intro = rwmb_meta( 'rw_intro');
 
                             if (!empty($intro)) {
-
                                 $excerpt = $intro;
-
                             } else {
-
                                 $excerpt = get_the_excerpt();
-
                             }
 
                             ?>
@@ -174,9 +166,7 @@
                         $small_banner_link_url = get_option("front_page_small_banner_link_url");
 
                         if (empty($small_banner_link_url)) {
-
                             $small_banner_link_url = "#";
-
                         }
 
                     } ?>
@@ -187,7 +177,7 @@
 
                         $newsletter_shortcode = get_option("front_page_newsletter_shortcode");
 
-                        if($newsletter_shortcode) {
+                        if ($newsletter_shortcode) {
 
                             ?>
 
@@ -226,21 +216,18 @@
                         $lastCol = '';
 
                         if ($i==0 || $i== 3 || $i==6) {
-
                             $lastCol = "last-col";
-
                         }
 
                         $args = array( 'posts_per_page' => 1, 'cat' => $cat_ID );
 
                         if ($cat_ID == $exclude_cat) {
-
                             $args['post__not_in'] = array($hero_post);
-
                         }
 
                     ?>
 
+                    <?php /* News categories */ ?>
                     <section class="m-all t-1of3 d-1of3 cf NewsCats <?php echo $lastCol?>" id="<?php echo $cat_name ?>">
 
                         <div class="cat">
@@ -250,9 +237,7 @@
                         $args = array( 'posts_per_page' => 4, 'cat' => $cat_ID );
 
                         if ($cat_ID == $exclude_cat) {
-
                             $args['post__not_in'] = array($hero_post);
-
                         }
 
                         query_posts($args);
@@ -267,21 +252,20 @@
 
                             $count = 0;
 
+                            /* Loop through categories */
                             while (have_posts()) : the_post();
 
                                 if ( in_array( $post->ID, $do_not_duplicate ) ) continue;
-
-                                $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'bones-thumb-450' );
-
+                                    $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'bones-thumb-450' );
                                 ?>
 
                                 <div class="entry">
 
-                                    <?php $count++; if ( $count % 4 == 1 ) { ?>
+                                    <?php $count++; if ( $count % 4 == 1 ) { /* Only show featured image for first post in category */ ?>
 
                                         <div class="cat-thumb" style="background-image: url(<?php echo $thumb[0]; ?>);"></div>
 
-                                    <?php } ?>
+                                    <?php } /* and for all sequential posts do not show featured image… */ ?>
 
                                     <h3>
                                         <a href="<?php echo the_permalink()?>" rel="bookmark" title="<?php echo the_title()?>">
@@ -290,9 +274,7 @@
                                             $tagline = rwmb_meta('rw_tagline');
 
                                             if ( !empty($tagline)) { ?>
-
                                                 <span><?php echo $tagline; _e(': '); ?> </span>
-
                                             <?php }
 
                                             echo the_title();
@@ -306,11 +288,8 @@
                                     $intro = rwmb_meta( 'rw_intro');
 
                                     if (!empty($intro)) {
-
                                         $excerpt = $intro;
-
                                     } else {
-
                                         $excerpt = get_the_excerpt();
                                     }
 
@@ -329,11 +308,8 @@
                             <?php
 
                             endif;
-
                             wp_reset_query();
-
                             echo category_description( $cat_ID);
-
                             $category_link = get_category_link( $cat_ID );
 
                             ?>
@@ -347,9 +323,7 @@
                     $i++;
 
                     if ($i==7) {
-
                         break;
-
                     }
 
                     endforeach;
@@ -369,25 +343,23 @@
 
                     if (!empty($banner_text)) {
 
-                    if (empty($banner_text_url)) {
-                        $banner_text_url = "#";
-                    }
+                        if (empty($banner_text_url)) {
+                            $banner_text_url = "#";
+                        }
 
-                    if (empty($banner_text_target)) {
-                        $banner_text_target = "_self";
-                    }
+                        if (empty($banner_text_target)) {
+                            $banner_text_target = "_self";
+                        } ?>
 
-                    ?>
+                        <a href="<?php echo $banner_text_url ?>" target="<?php echo $banner_text_target ?>" title="<?php echo $banner_text ?>">
+                            <div class="mid-home-banner m-all cf">
+                                <h2><?php echo $banner_text ?></h2>
+                            </div>
+                        </a>
 
-                    <a href="<?php echo $banner_text_url ?>" target="<?php echo $banner_text_target ?>" title="<?php echo $banner_text ?>">
-                        <div class="mid-home-banner m-all cf">
-                            <h2><?php echo $banner_text ?></h2>
-                        </div>
-                    </a>
+                    <?php } ?>
 
                 </section>
-
-                <?php } ?>
 
             </main>
 
